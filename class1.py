c3712353ef2bb962f53999b23ffce26e4d58b2de
Mk1el <moseskilonzo2001@gmail.com>
2024-08-24 14:40:45 +0300

    Sat Trial

---
+++ class1.py
@@ -0,0 +1,34 @@
+#Let's store a favourite number
+#Defining a variable. It should 
+# Favourite_Number = 73
+# print(f"My favourite number is: {Favourite_Number}")
+
+#String
+# greeting = "Hello, Welcome to our class"
+# print(greeting)  
+
+# #Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f'Is python fun?{is_python_fun}')
+# print(f'Is python boring? {is_python_boring}')
+
+#Control statement
+# temperature = 30
+# if temperature > 25:
+#     print(f"It is hot outside! Wear shorts.")
+# elif temperature >= 15:
+#     print("It is warm. Wear a light jacket.")
+# else :
+#     print("Brr! It's cold. Bundle up!")
+
+#Module
+#In-built Module
+# import math
+# result = math.sqrt(81)
+# print(f'The square root of 81 is:{result}')
+
+import greet
+print(greet.greet("Mark"))
+print(greet.greet("Messi"))
+
